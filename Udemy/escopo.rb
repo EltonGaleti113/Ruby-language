b88cd6897074331e85dcb943a71ac53d969d572f
Elton Galeti <egaleti2003@gmail.com>
2024-05-22 11:11:20 -0300

    Added escopo.rb file

---
+++ Udemy/escopo.rb
@@ -0,0 +1,11 @@
+#Escopo em ruby
+#
+#
+
+nome = 'Elton'
+
+def nome_completo
+    nome = 'Elton Galeti'
+end
+    #imprime o nome fora da classe, porque a variavel nome = elton, está em escopo global
+    puts nome
